48e2c842919c40f7c0dab29f2555a9c7907ec9a7
sadikahmetaydin <sadikahmetaydin@gmail.com>
2024-06-11 22:07:28 +0300

    Crypto Price Tracking App Final

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -1,4 +1,5 @@
-import React from 'react';
+import React, { useContext } from 'react';
+import { CoinContext } from '../../context/CoinContext';
 // Style Component
 import './Navbar.css';
 // Images Connections
@@ -6,6 +7,29 @@ import logo from '../../assets/logo.png';
 import arrow_icon from '../../assets/arrow_icon.png';
 
 const Navbar = () => {
+  const { setCurrency } = useContext(CoinContext);
+
+  const currencyHandler = (event) => {
+    switch (event.target.value) {
+      case 'usd': {
+        setCurrency({ name: 'usd', symbol: '$' });
+        break;
+      }
+      case 'eur': {
+        setCurrency({ name: 'eur', symbol: '€' });
+        break;
+      }
+      case 'try': {
+        setCurrency({ name: 'try', symbol: '₺' });
+        break;
+      }
+      default: {
+        setCurrency({ name: 'usd', symbol: '$' });
+        break;
+      }
+    }
+  };
+
   return (
     <div className="navbar">
       <img className="logo" src={logo} alt="" />
@@ -18,7 +42,7 @@ const Navbar = () => {
       </ul>
 
       <div className="nav-right">
-        <select>
+        <select onChange={currencyHandler}>
           <option value="usd">USD</option>
           <option value="eur">EUR</option>
           <option value="try">TRY</option>
